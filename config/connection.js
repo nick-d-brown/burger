b98649e4554646419401c01f504eccdd549d9cdf
Nick Brown <n.brown.professional@gmail.com>
2018-05-02 18:01:44 -0600

    minor changes to css and attempts to fix heroku connection issue

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,19 @@
 
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "runthisCODE",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "runthisCODE",
+        database: "burgers_db"
+    });
+}
+
+
 
 connection.connect(function (err) {
     if (err) {
